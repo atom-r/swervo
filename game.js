4b304ad267c292a080567fa12406ed3efe81fa64
Adam Richard <arichard44@gmail.com>
2017-03-03 15:16:24 -0500

    add text

--- game.js
+++ game.js
@@ -52,9 +52,32 @@
 	    this.stage = new createjs.Stage("myCanvas");
 	    this.corridor = new Corridor(this.stage);
 
+	    this.buildCpuText();
+	    this.buildHumanText();
+
 	    this.cpuStrikes = 3;
 	    this.humanStrikes = 5;
 	    this.level = 1;
+	  }
+
+	  buildCpuText() {
+	    const text = new createjs.Text("CPU", "20px Arial", "#FFF8F0");
+	    text.x = 100;
+	    text.y = 70;
+	    text.textBaseline = "alphabetic";
+
+	    this.stage.addChild(text);
+	    this.stage.update();
+	  }
+
+	  buildHumanText() {
+	    const text = new createjs.Text("Player", "20px Arial", "#FFF8F0");
+	    text.x = 650;
+	    text.y = 70;
+	    text.textBaseline = "alphabetic";
+
+	    this.stage.addChild(text);
+	    this.stage.update();
 	  }
 	}
 
